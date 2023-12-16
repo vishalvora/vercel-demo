a6cc9745a37f3fef1f775fd2fd4565f39aff2ad7
Vishal Vora <vvora@enovix.com>
2023-12-16 10:50:18 +0530

    1st commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Home Page Route'
+
+
+@app.route('/about')
+def about():
+    return 'About Page Route'
+
+
+@app.route('/portfolio')
+def portfolio():
+    return 'Portfolio Page Route'
+
+
+@app.route('/contact')
+def contact():
+    return 'Contact Page Route'
+
+app.run()
+
+
